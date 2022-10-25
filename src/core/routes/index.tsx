989a633ead50d760f874ae5d5f4844e1c3bf596f
firstChairCoder <md1qq7@gmail.com>
2022-10-25 05:17:36 +0100

    chore: initial commit

---
+++ src/core/routes/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+import { Home, Question, Result } from '@src/modules/quiz/presenter';
+
+const Stack = createStackNavigator();
+
+export const Routes: React.FC = () => (
+    <Stack.Navigator>
+      <Stack.Screen
+        name='Home'
+        component={Home}
+        options={{ headerShown: false }}
+        />
+      <Stack.Screen
+        name='Question'
+        component={Question}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name='Result'
+        component={Result}
+        options={{ headerShown: false }}
+      />
+    </Stack.Navigator>
+)
